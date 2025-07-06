5d5881a6204781b9749272e6778648388f7f9715
Moon <muhammadmuneebulraza@gmail.com>
2025-07-06 01:13:46 -0700

    not null constraint

---
+++ Topic2_NotNull_constraint/Topic2_NotNull_Insert_value.sql
@@ -0,0 +1,6 @@
+insert into not_null
+values
+("Muneeb","02323"),
+("","33333"),
+("",null),
+("Muneeb","");
---
+++ Topic2_NotNull_constraint/Topic2_NotNull_create_table.sql
@@ -0,0 +1,4 @@
+create table NOT_NULL (
+     name varchar(30) Not NULL ,
+     phone varchar(11)
+)
